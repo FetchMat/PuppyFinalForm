94249bc038be8ad9c97460797c08ef47e2cb4bfe
FetchMaat <mat.rollerstyle@live.fr>
2023-12-01 20:51:48 +0100

    token reset

--- deploy/global.js
+++ deploy/global.js
@@ -5,14 +5,14 @@ const LoadCommands = require("../util/loadCommands");
 
 (async () => {
 	const config = await getConfig();
-	const rest = new REST({ version: "9" }).setToken('MTE0OTY4MDU3NDYyNjI4MzUyMA.GxYwcU.lU9oY8cnChg6Bi-5rmlhmVsqQejyxT4hADSJ_U');
+	const rest = new REST({ version: "9" }).setToken(process.env.token);
 	const commands = await LoadCommands().then((cmds) => {
 		return [].concat(cmds.slash).concat(cmds.context);
 	});
 	
 	console.log("Deploying commands to global...");
 	await rest
-		.put(Routes.applicationCommands('1149680574626283520'), {
+		.put(Routes.applicationCommands(process.env.clientId), {
 			body: commands,
 		})
 		.catch(console.log);
