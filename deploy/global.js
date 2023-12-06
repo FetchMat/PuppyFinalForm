a5657dbc9f7d89238dafef334a39ed4265f70169
FetchMaat <mat.rollerstyle@live.fr>
2023-12-06 20:48:44 +0100

    puppy modif

--- deploy/global.js
+++ deploy/global.js
@@ -5,14 +5,14 @@ const LoadCommands = require("../util/loadCommands");
 
 (async () => {
 	const config = await getConfig();
-	const rest = new REST({ version: "9" }).setToken(process.env.token);
+	const rest = new REST({ version: "9" }).setToken(config.token);
 	const commands = await LoadCommands().then((cmds) => {
 		return [].concat(cmds.slash).concat(cmds.context);
 	});
 	
 	console.log("Deploying commands to global...");
 	await rest
-		.put(Routes.applicationCommands(process.env.clientId), {
+		.put(Routes.applicationCommands(config.clientId), {
 			body: commands,
 		})
 		.catch(console.log);
