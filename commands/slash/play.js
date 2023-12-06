e03a3425bcce41eb8489871d4aa82f76b44c2f55
FetchMaat <mat.rollerstyle@live.fr>
2023-12-06 22:18:51 +0100

    presque

--- commands/slash/play.js
+++ commands/slash/play.js
@@ -20,7 +20,7 @@ const command = new SlashCommand()
 
     let player;
     if (client.manager) {
-      player = client.createPlayer(interaction.channel, channel);
+      player = client.createPlayer(interaction.channel, channel);      
     } else {
       return interaction.reply({
         embeds: [
@@ -37,11 +37,11 @@ const command = new SlashCommand()
 
     if (channel.type == "GUILD_STAGE_VOICE") {
       setTimeout(() => {
-        if (interaction.guild.me.voice.suppress == true) {
+        if (interaction.guild.members.voice.suppress == true) {
           try {
-            interaction.guild.me.voice.setSuppressed(false);
+            interaction.guild.members.voice.setSuppressed(false);
           } catch (e) {
-            interaction.guild.me.voice.setRequestToSpeak(true);
+            interaction.guild.members.voice.setRequestToSpeak(true);
           }
         }
       }, 2000); // Need this because discord api is buggy asf, and without this the bot will not request to speak on a stage - Darren
